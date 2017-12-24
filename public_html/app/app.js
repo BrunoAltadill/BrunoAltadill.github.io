cd6efc49e25eef7913679d5f5728f7d97a6defa7
Thyplock <brualgo@gmail.com>
2017-12-24 14:51:27 +0100

    First iteration...

--- public_html/app/app.js
+++ public_html/app/app.js
@@ -5,17 +5,27 @@ define(['jquery', 'typewriter'], function ($, typewrite) {
 
         //Init title page
         self.init = function () {
+            //Presentation of webpage
+            homePage();
+        };
+
+        //Show presentation
+        function homePage() {
             $('#title').typewrite({
+                blinkingCursor: false,
                 actions: [
-                    {type: 'Bruno Altadill Gonzalez'},
+                    {type: 'Bruno Altadill Gonzalez '},
                     {delay: 1500},
-                    {remove: {num: 23, type: 'stepped'}},
+                    {type: '<br>'},
+                    {type: 'Informatics Engineer'},
+                    {delay: 1500},
+                    {remove: {num: 20, type: 'stepped'}},
                     {delay: 1500},
                     {type: 'Work in progress. Please be patient...'},
                     {delay: 10000}
                 ]
             });
-        };
+        }
     }
 
     return new Model();
